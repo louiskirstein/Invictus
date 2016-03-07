045411bb6b092d77129523687cef5e11ddc7feab
louiskirstein <louis.kirstein@brentwood.ca>
2016-03-07 12:20:47 -0800

    first change

--- src/invictus/Invictus.java
+++ src/invictus/Invictus.java
@@ -15,7 +15,7 @@ public class Invictus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("dont do that");
+        System.out.println("dont do that, again");
     }
     
 }
