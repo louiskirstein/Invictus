f3b1dd6f085560a9801d0e45d31e9035832f1351
louiskirstein <louis.kirstein@brentwood.ca>
2016-04-07 09:15:09 -0700

    path

--- src/invictus/World.java
+++ src/invictus/World.java
@@ -6,30 +6,47 @@
 package invictus;
 
 import environment.Environment;
+import environment.MovableIntf;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import path.Path;
+import path.PathEventHandler;
+import path.PathEventType;
 import path.PathSegment;
 
 /**
  *
  * @author Nick
  */
-class World extends Environment {
+class World extends Environment implements PathEventHandler {
     
     Path path;
 
     public World() {
         ArrayList<PathSegment> segments = new ArrayList<>();
-        segments.add(new PathSegment(0, 100, 50, 100));
-        segments.add(new PathSegment(50, 100, 50, 200));
-//        segments.add(new PathSegment(new Point(0, 100), new Point(50, 100)));
-//        segments.add(new PathSegment(new Point(50, 100), new Point(50, 200)));
+        segments.add(new PathSegment(0, 150, 100, 150));
+        segments.add(new PathSegment(100, 150, 100, 450));
+        segments.add(new PathSegment(100, 450, 300, 450));
+        segments.add(new PathSegment(300, 450, 300, 70));
+        segments.add(new PathSegment(300, 70, 500, 70));
+        segments.add(new PathSegment(500, 70, 500, 200));
+        segments.add(new PathSegment(500, 200, 700, 200));
+        segments.add(new PathSegment(700, 200, 700, 70));
+        segments.add(new PathSegment(700, 70, 830, 70));
+        segments.add(new PathSegment(830, 70, 830, 400));
+        segments.add(new PathSegment(830, 400, 550, 400));
+        segments.add(new PathSegment(550, 400, 550, 600));
+
+
         
+
+
+
         path = new Path(segments);
+        
     }
 
     @Override
@@ -58,5 +75,12 @@ class World extends Environment {
             segment.draw(graphics);
         }
     }
+
+//<editor-fold defaultstate="collapsed" desc="PathEventHandler">
+    @Override
+    public void pathEvent(PathEventType pathEventType, MovableIntf mover) {
+        
+    }
+//</editor-fold>
     
 }
